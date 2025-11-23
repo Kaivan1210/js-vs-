ecb8bab9539788babfe192e13ab51b2cd69fb1d9
Shah Kaivan HiteshBhai <kaivanshah2006@gmail.com>
2025-11-23 10:14:43 +0000

    container config

--- .devcontainer/01_basics/variable.js
+++ .devcontainer/01_basics/variable.js
@@ -2,7 +2,8 @@ const accountId = 25310100045250
 let accountEmail = "KaivanShah2006@gmail.com"
 let accountPassword = "5102"
 accountCity = "Ahmedabad"
+let accountState
 
 console.log(accountId);
 
-console.table([accountId , accountEmail , accountPassword , accountCity])
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
