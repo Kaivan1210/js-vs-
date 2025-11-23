5559e0a63abbc45d9345a4304e54d4ef3e434cf3
Shah Kaivan HiteshBhai <kaivanshah2006@gmail.com>
2025-11-23 10:36:31 +0000

    container config

--- .devcontainer/01_basics/datatype.js
+++ .devcontainer/01_basics/datatype.js
@@ -3,3 +3,27 @@ console.log("Kaivan Shah")
 let age = 19
 console.log("Age is:", age)
 
+console.log(typeof age)
+console.log(typeof "Kaivan Shah")
+console.log(typeof true)
+console.log(typeof 19.5)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {name: "Kaivan", age: 19})
+console.log(typeof [1,2,3,4,5])
+
+let isStudent = true
+console.log(typeof isStudent)
+let marks = null
+console.log(typeof marks)
+
+let score
+console.log(typeof score)
+score = 95.5
+console.log(typeof score)
+console.log("Score is:", score)
+
+const pi = 3.14
+console.log("Value of pi is:", pi)
+
+
